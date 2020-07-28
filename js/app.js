386dca4bb356be248b94c824c669c65bed0d72ac
MhRantz <meganrantz@gmail.com>
2020-07-28 12:50:48 -0400

    urls are awesome now

--- js/app.js
+++ js/app.js
@@ -4,19 +4,18 @@
 // If they change genre, there will be a new API call for that genre
 // Return results to the grid
 // Create a dropdown option for All Games to bring back the initial general list of games to the grid
-
 // Declare namespace
 const app = {}
-
 app.selectionGenre = "";
 app.currentDate = new Date();
-app.todaysDate = `${app.currentDate.getFullYear()}-${app.currentDate.getMonth()+1}-${app.currentDate.getDate()}`;
+app.todaysDate = `${app.currentDate.getFullYear()}-${("0" + (app.currentDate.getMonth() + 1)).slice(-2)}-${("0" + (app.currentDate.getDate())).slice(-2)}`;
+app.futureDate = `${app.currentDate.getFullYear() + 1}-${("0" + (app.currentDate.getMonth() + 1)).slice(-2)}-${("0" + (app.currentDate.getDate())).slice(-2)}`;
 app.games = [];
-app.url = `https://api.rawg.io/api/games?dates=${app.todaysDate},2021-04-01`
+app.urlGen = `https://api.rawg.io/api/games?dates=${app.todaysDate},${app.futureDate}`
 // API Calls
-app.apiGeneral = function() {
+app.apiGeneral = function(i){
     $.ajax({
-        url: `https://api.rawg.io/api/games?dates=2020-07-27,2021-07-27`,
+        url: i,
         method: 'GET',
         dataType: 'json',
         headers: {
@@ -35,40 +34,38 @@ app.apiGeneral = function() {
 
 app.gridCreation = function(){
     $('.games').empty();
-   //results array comes in and then we for each them into template literals appended the games ul.  
+//results array comes in and then we for each them into template literals appended the games ul.  
     $.each(app.games, function(){
+        const gamePlatforms = this.parent_platforms.map((i)=> {return i.platform.slug}).join(", ");
+        const gameGenres = this.genres.map((i)=>{return i.name}).join(", ");
         $('.games').append(
             `<li>
                 <div style="background-image:url(${this.background_image}")>
                     <h3>${this.name}</h3>
                 </div>
                 <time datetime="${this.released}">${this.released}</time>
-                <p>GENRES</p>
-                <ul><li>PLATFORMS</li><ul>
+                <p>${gameGenres}</p>
+                <ul><li>${gamePlatforms}</li><ul>
             </li>`
         );
     });
-   
-
-    //<li><div background-image><h3>TITLE</h3></div>p release date list of genres Date- list of icons platforms </li>
 }
 
 // Event Listeners
 app.selectionListener = function() {
     $('.genreSelect').on('change', function () {
         app.selectionGenre = $(this).val();
+        let urlGenre = `https://api.rawg.io/api/games?dates=${app.todaysDate},${app.futureDate}&genres=${$(this).val()}`;
+        app.apiGeneral(urlGenre);
+        console.log(urlGenre);
     });
 }
-
 // Init
 app.init = function() {
     app.selectionListener();
-    app.apiGeneral();
-    
+    app.apiGeneral(app.urlGen);
 }
-
 // Document ready
-
 $(function(){
     app.init();
 })
